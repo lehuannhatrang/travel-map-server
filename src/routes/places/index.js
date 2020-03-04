203f6f1eb1b27de2f12f007f731ac45d9b1b3356
Le Huan <lehuannhatrang98@gmail.com>
2020-03-04 10:07:55 +0700

    update

--- src/routes/places/index.js
+++ src/routes/places/index.js
@@ -32,10 +32,9 @@ PlaceRouter.get('/criterial-base/list', (req, res) => {
             HttpUtil.makeErrorResponse(res, 500)
             return;
         }
-        // const result = stdout.split(']').join().split('[').join().split(' ').filter(ele => !!ele && ele !== ',')
-        const result = stdout
+        const result = stdout.split(',')
         console.log(`${result}`);
-        RestaurantInfoModel.getByQuery({restaurantId: {$in:[4827,3807,10885,11986,10615,5670,10699,5190,12567,7669] }})
+        RestaurantInfoModel.getByQuery({restaurantId: {$in:[ 4827,3807,10885,11986,10615,5670,10699,5190,12567,7669] }})
         .then(result => {
             console.log(result)
         })
